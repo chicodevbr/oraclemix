354af8a25c31b7cb134871e14f982514b71d2840
F. Júnior <chicodevbr@gmail.com>
2021-04-07 15:47:09 -0300

    first peace of code

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const quotesData = ['You should have seen me reading Marx', 'Spam! Spam! Spam! Spam! Spam! Spam!', 
+    'Let me sleep all night in your soul kitchen', 'Follow the weird naked Indian' ]
+
+const personaData = ['The Pope', 'a mockingbird', 'a vampire', 'Mark Zuckerberg', 'a snake', 'Jim Morrison'];
+
+const verbData = ['see', 'talk with', 'dream of'];
+
+const actionData = ['drinkin a tea', 'crossing the streets', 'crawling', 'flying in the skies of london'];
+
+const luckData = ['then you will win the lottery', 'then you will find great love', 'so you have a broken heart', 'then you will have a trip'];
+
+const watchOutData = ['a crow can land on your window', 'a whale may appear in your pool', 'a bear can show up for dinner']
+
+
+
+
+
+//if you see the pope drinking tea, then you will win the lottery.
+
+
+let fullstring = `If you ${verb} ${persona} ${action}, than you ${luck}, but be careful ${watchOut}. Your lucky phrase is: ${quotes}.`
+ 
+
